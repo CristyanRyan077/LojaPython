12a32a1a1b3ab36d93626620e0b7d2dd7dffa5f9
cristyan <crisryan077@gmail.com>
2025-05-25 19:05:11 -0300

    teste

--- estoque/models.py
+++ estoque/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class produto(models.Model):
     nome = models.CharField(max_length=100)
-    quantidade = models.IntegerField(max_length=100)
+    quantidade = models.IntegerField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
