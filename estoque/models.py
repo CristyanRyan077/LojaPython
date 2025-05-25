2e507b7f23852155ce456914a22a43c11180c649
cristyan <crisryan077@gmail.com>
2025-05-25 19:02:21 -0300

    teste

--- estoque/models.py
+++ estoque/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 
 class produto(models.Model):
-    nome = models.CharField(max_length=255)
-    quantidade = models.IntegerField()
+    nome = models.CharField(max_length=100)
+    quantidade = models.IntegerField(max_length=100)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"{self.nome} - {self.quantidade} unidades - R${self.preco:.2f}"
-    
+
+
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     telefone = models.CharField(max_length=20)
