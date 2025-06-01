a263d55e9cb13ab0e363a07ad0ae3fc941efee47
cristyan <crisryan077@gmail.com>
2025-06-01 13:35:42 -0300

    adicionado caminho para imagems

--- estoque/models.py
+++ estoque/models.py
@@ -7,6 +7,8 @@ class produto(models.Model):
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_modificacao = models.DateTimeField(auto_now=True)
+    descricao = models.CharField(max_length=100)
+    imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
 
     def __str__(self):
         return f"{self.nome} - {self.quantidade} unidades - R${self.preco:.2f}"
